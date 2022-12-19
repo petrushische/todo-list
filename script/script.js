78487674af814231e4b9e80be61f5fd6dcee5b8b
Пётр Румянцев <dogboxer64529000@gmail.com>
2022-12-19 21:29:02 +0300

    fix/Выбор всех дел

--- script/script.js
+++ script/script.js
@@ -1,25 +1,20 @@
+// Данные
 const container = document.querySelector('.container-all');
-const containerActiv = document.querySelector('.container-active');
-const containerComplete = document.querySelector('.container-completed');
-
 const template = document.querySelector('.template').content;
-
 const buttonWrapper = document.querySelector('.button-wrapper');
-
 const count = document.querySelector('.count');
-
 const fullDone = document.querySelector('.full-done');
-
 const todoList = container.children
-
 const buttonClear = buttonWrapper.querySelector('.button-conteiner-clear')
-
 const buttonDefault = document.querySelector('.button-conteiner-all');
 const buttonActive = document.querySelector('.button-conteiner-active');
 const buttonComplet = document.querySelector('.button-conteiner-completed');
+// localStorage
 
 
-function renderCompleted() {
+/*
+// Фильтры
+function filters() {
  Array.from(todoList).forEach((elem) => {
   if (window.location.href === 'http://127.0.0.1:5500/index.html#/') {
    elem.style = 'display:flex'
@@ -36,73 +31,26 @@ setInterval(function () {
   buttonDefault.classList.add('button-conteiner-on')
   buttonActive.classList.remove('button-conteiner-on')
   buttonComplet.classList.remove('button-conteiner-on')
-  console.log(window.location.href)
  } else if (window.location.href === 'http://127.0.0.1:5500/index.html#/active') {
   buttonActive.classList.add('button-conteiner-on')
   buttonDefault.classList.remove('button-conteiner-on')
   buttonComplet.classList.remove('button-conteiner-on')
-  console.log(window.location.href)
  } else if (window.location.href === 'http://127.0.0.1:5500/index.html#/completed') {
   buttonComplet.classList.add('button-conteiner-on')
   buttonActive.classList.remove('button-conteiner-on')
   buttonDefault.classList.remove('button-conteiner-on')
-  console.log(window.location.href)
  }
- renderCompleted()
-}, 10);
+ filters()
+}, 100);
+*/
 
-
-
-
-
-function countTodoli() {
- let countLi = todoList.length
- return countLi
-}
-
-function clear() {
- let counterCheckDelo = 0;
- Array.from(todoList).forEach((elem) => {
-  elem.classList.contains('check-delo') ? counterCheckDelo += 1 : false;
- })
- if (counterCheckDelo >= 1) {
-  buttonClear.classList.add('button-conteiner-clear-active')
- } else {
-  buttonClear.classList.remove('button-conteiner-clear-active')
- }
- count.textContent = `${countTodoli() - counterCheckDelo} item`
-}
-
-function showButton() {
- if (countTodoli() > 0) {
-  buttonWrapper.classList.add('button-wrapper-visible')
-  count.textContent = `${countTodoli()}item`
-  fullDone.classList.add('full-done-visible')
- } else {
-  buttonWrapper.classList.remove('button-wrapper-visible')
-  count.textContent = ''
-  fullDone.classList.remove('full-done-visible')
- }
-}
-
-function checkCheked(elem) {
- if (elem.checked) {
-  elem.closest('.todo-li').classList.add('check-delo');
- } else {
-  elem.closest('.todo-li').classList.remove('check-delo');
- }
-
-}
-
+// Создание задачи
 function renderTemplate(item) {
  const element = template.querySelector('.todo-li').cloneNode(true);
-
  const paragr = element.querySelector('.case');
- paragr.id = item
+ paragr.id = Date.now()
  paragr.textContent = item;
-
  const inputChange = element.querySelector('.todo-change')
-
  const buttonInput = element.querySelector('.input-save')
  buttonInput.addEventListener('click', () => {
   paragr.textContent = inputChange.value
@@ -111,20 +59,22 @@ function renderTemplate(item) {
   if (inputChange.value === '') {
    element.remove()
   }
-  showButton()
+  showButton();
  })
 
  const trash = element.querySelector('.trash');
  trash.addEventListener('click', (evt) => {
   evt.target.closest('.todo-li').remove();
   showButton();
-  countTodoli()
+  countTodoli();
+
  })
 
  const done = element.querySelector('.done');
  done.addEventListener('click', () => {
   checkCheked(done);
   clear();
+  fullCheckTodo();
 
  })
 
@@ -138,14 +88,14 @@ function renderTemplate(item) {
   buttonInput.classList.add('input-save-visible')
   inputChange.value = paragr.textContent
  })
+
  return element
 }
-
-
+// Рендер задачи
 function render(item) {
  container.append(renderTemplate(item))
 }
-
+// рендер задачи с данными из инпута формы
 const form = document.querySelector('.form');
 const input = form.querySelector('.name');
 form.addEventListener('submit', (evt) => {
@@ -153,46 +103,78 @@ form.addEventListener('submit', (evt) => {
  render(input.value)
  form.reset();
  showButton();
- countTodoli()
+ countTodoli();
+
 })
 
-// 
-
-function CallBack(evt) {
- if (evt.target.checked === false) {
-  fullDone.checked = false
- } else {
- }
+// счетчик дел
+function countTodoli() {
+ let countLi = todoList.length;
+ return countLi
 }
 
-fullDone.addEventListener('click', () => {
- Array.from(todoList).forEach((element) => {
-  const check = element.querySelector('.done')
-  if (fullDone.checked === true) {
-   check.checked = true;
-   check.addEventListener('click', CallBack)
-  } else if (fullDone.checked === false) {
-   check.removeEventListener('click', CallBack)
-   check.checked = false;
-  }
-  checkCheked(check);
-
- })
- clear();
-})
-
-
-
-
-buttonClear.addEventListener('click', () => {
+// Показывать/Скрывать кнопку удаления задач и обработчик клика
+function clear() {
+ let counterCheckDelo = 0;
  Array.from(todoList).forEach((elem) => {
-  elem.classList.contains('check-delo') ? elem.remove() : buttonClear.classList.remove('button-conteiner-clear-active')
+  elem.classList.contains('check-delo') ? counterCheckDelo += 1 : false;
+  buttonClear.addEventListener('click', () => {
+   elem.classList.contains('check-delo') ? elem.remove() : buttonClear.classList.remove('button-conteiner-clear-active')
+   showButton();
+
+  })
  })
- if (countTodoli() === 0) {
+ counterCheckDelo >= 1 ? buttonClear.classList.add('button-conteiner-clear-active') : buttonClear.classList.remove('button-conteiner-clear-active');
+ count.textContent = `${countTodoli() - counterCheckDelo} item`;
+}
+
+// Показывать/Скрывать Панель с фильтрами,счетчиком 
+function showButton() {
+ if (countTodoli() > 0) {
+  buttonWrapper.classList.add('button-wrapper-visible')
+  count.textContent = `${countTodoli()}item`
+  fullDone.classList.add('full-done-visible')
+ } else {
   buttonWrapper.classList.remove('button-wrapper-visible')
   count.textContent = ''
   fullDone.classList.remove('full-done-visible')
   buttonClear.classList.remove('button-conteiner-clear-active')
  }
- showButton()
+}
+// Функция проверки отметки задачи
+function checkCheked(elem) {
+ if (elem.checked) {
+  elem.closest('.todo-li').classList.add('check-delo');
+ } else {
+  elem.closest('.todo-li').classList.remove('check-delo');
+ }
+}
+
+// Чекбокс отметки всех задач
+fullDone.addEventListener('click', () => {
+ Array.from(todoList).forEach((element) => {
+  const check = element.querySelector('.done')
+  if (fullDone.checked === true) {
+   check.checked = true;
+  } else if (fullDone.checked === false) {
+   check.checked = false;
+  }
+  checkCheked(check);
+ })
+ fullCheckTodo();
+ clear();
+
 })
+
+// Все задачи отмечены/ничего не отмечено
+function fullCheckTodo() {
+ let countCompletTodo = 0;
+ Array.from(todoList).forEach((el) => {
+  el.classList.contains('check-delo') ? countCompletTodo += 1 : false
+ })
+ if (countCompletTodo !== countTodoli()) {
+  fullDone.checked = false
+ } else {
+  fullDone.checked = true
+ }
+}
